628abf138f2f5acc256c7d170bcde1c41f1901fd
Md. Al-Amin Mia <md.alamin.mia.webdev@gmail.com>
2025-11-26 22:40:22 +0600

    N

--- index.js
+++ index.js
@@ -6,16 +6,11 @@ require('dotenv').config()
 const port = process.env.PORT || 3000
 
 // Middleware
-app.use(express.json())   
-app.use(cors())   
-
-
+app.use(express.json())
+app.use(cors())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.1rbama4.mongodb.net/?appName=Cluster0`;
 
-
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
@@ -26,11 +21,12 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
+    const db = client.db("Rapido-Data-Base");
+    const parcelCollection = db.collection("Parcel-Sending-Info");
 
-     // POST API → add new parcel
+    // POST API → add new parcel
     app.post("/parcels", async (req, res) => {
       const newParcel = req.body;
       const result = await parcelCollection.insertOne(newParcel);
@@ -39,24 +35,28 @@ async function run() {
 
     // GET API → fetch all parcels
     app.get("/parcels", async (req, res) => {
-      const parcels = await parcelCollection.find().toArray();
-      res.send(parcels);
+
+      const query = {};
+      const {email} = req.query;
+
+      if(email){
+        query.senderEmail = email; 
+      }
+      const cursor = parcelCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+      
+
     });
 
-
-
-
-
-
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
+    console.log("MongoDB Connected");
+  } catch (err) {
+    console.log(err);
   }
 }
 run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
@@ -64,5 +64,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
